e21a3703e8fe92beb3c85e16e6aa6717e11bb972
Adarsh <Adarshbunny293@gmail.com>
2024-03-10 22:50:49 +0530

    Added Automated sidebar

--- source/conf.py
+++ source/conf.py
@@ -59,6 +59,9 @@ html_context = {
     "github_version":"main",
     "doc_path": "docs",
 }
+html_sidebars = {
+   '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'],
+}
 
 # Add any paths that contain custom static files (such as style sheets) here, relative to this directory. They are copied after the builtin static files, so a file named "default.css" will overwrite the builtin "default.css"
 html_static_path = ['_static']
